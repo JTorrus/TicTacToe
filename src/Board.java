e981e620a8b7e94d8a94bf38e8f2b0514e951280
dementedapple5 <danihabana96@gmail.com>
2017-12-04 20:05:44 +0100

    Getters, setters and constructor of Board created

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,15 @@
 public class Board {
     private Cell[][] cells;
+
+    public Board(Cell[][] cells) {
+        this.cells = cells;
+    }
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public void setCells(Cell[][] cells) {
+        this.cells = cells;
+    }
 }
