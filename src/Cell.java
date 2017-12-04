a514dc5ac35076d412867b6b63ac45d2ee7171e3
dementedapple5 <danihabana96@gmail.com>
2017-12-04 20:03:38 +0100

    Getters, setters and constructor of Cell created

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,25 @@
 public class Cell {
     private boolean inUse;
     private Player player;
+
+    public Cell(boolean inUse, Player player) {
+        this.inUse = inUse;
+        this.player = player;
+    }
+
+    public boolean isInUse() {
+        return inUse;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
