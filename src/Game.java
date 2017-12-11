1e5171b58a7dd6c938f476f979be1c99976f43a8
JTorrus <jtorrusgalindo@gmail.com>
2017-12-11 01:28:46 +0100

    Main restructured, Board class renamed to Game, now players can play again until they choose to finish.

---
+++ src/Game.java
@@ -0,0 +1,127 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game {
+    private Cell[][] cells;
+    private ArrayList<Player> players;
+
+    public Game() {
+        this.cells = createBoard();
+        this.players = insertPlayers();
+    }
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public Cell[][] createBoard() {
+        int id = 1;
+        Cell[][] cells = new Cell[3][3];
+
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell(false, id);
+                id++;
+            }
+        }
+
+        return cells;
+    }
+
+    public void showBoard() {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                System.out.print(cells[i][j].getForm() + " ");
+            }
+
+            System.out.println(" ");
+        }
+    }
+
+    public ArrayList<Player> insertPlayers() {
+        Scanner sc = new Scanner(System.in);
+        Player player1;
+        Player player2;
+        String auxName;
+        ArrayList<Player> players = new ArrayList<>();
+
+        System.out.print("Insert first player's name: ");
+        auxName = sc.nextLine();
+        player1 = new Player(auxName, "O");
+
+        System.out.print("Insert second player's name: ");
+        auxName = sc.nextLine();
+        player2 = new Player(auxName, "X");
+
+        players.add(player1);
+        players.add(player2);
+
+        return players;
+    }
+
+    public boolean checkFinish(Player player) {
+        boolean finish = false;
+        ArrayList<Integer> cellIdList = new ArrayList<>();
+
+        if (player.getCellsQty() >= 3) {
+            for (Cell cell : player.getCellsSelected()) {
+                cellIdList.add(cell.getId());
+            }
+            if ((cellIdList.contains(1) && cellIdList.contains(2) && cellIdList.contains(3)) || (cellIdList.contains(4) && cellIdList.contains(5) && cellIdList.contains(6)) || (cellIdList.contains(7) && cellIdList.contains(8) && cellIdList.contains(9))) {
+                finish = true;
+                player.addVictory();
+                System.out.println("Congratulations, " + player.getName() + " won!!!");
+            } else if ((cellIdList.contains(1) && cellIdList.contains(4) && cellIdList.contains(7)) || (cellIdList.contains(2) && cellIdList.contains(5) && cellIdList.contains(8)) || (cellIdList.contains(3) && cellIdList.contains(6) && cellIdList.contains(9))) {
+                finish = true;
+                player.addVictory();
+                System.out.println("Congratulations, " + player.getName() + " won!!!");
+            } else if ((cellIdList.contains(1) && cellIdList.contains(5) && cellIdList.contains(9)) || (cellIdList.contains(3) && cellIdList.contains(5) && cellIdList.contains(7))) {
+                finish = true;
+                player.addVictory();
+                System.out.println("Congratulations, " + player.getName() + " won!!!");
+            }
+
+        }
+
+        return finish;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void startGame() {
+        Scanner sc = new Scanner(System.in);
+        String restart = "";
+        this.cells = createBoard();
+        int counter = 0;
+        boolean finish = false;
+
+        for (Player player: players) {
+            player.getCellsSelected().clear();
+        }
+
+        while (!finish) {
+            for (Player current : this.getPlayers()) {
+                System.out.println("Turn for " + current.getName());
+                current.makePlay(this);
+                if (this.checkFinish(current)) {
+                    finish = true;
+                    System.out.println("Do you want to play again? (y/n)");
+                    restart = sc.nextLine();
+                    break;
+                }
+                counter++;
+                if (counter == 9) {
+                    finish = true;
+                    System.out.println("It's a tie, congratulations to both players");
+                    System.out.println("Do you want to play again? (y/n)");
+                    restart = sc.nextLine();
+                    break;
+                }
+            }
+        }
+
+        if (restart.equalsIgnoreCase("y")) startGame();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,72 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
-        ArrayList<Player> players = insertPlayers();
-        Board board = new Board(players.get(0), players.get(1));
-        int counter = 0;
-        boolean finish = false;
+        Game game = new Game();
 
-        board.createBoard();
-        board.showBoard();
-
-        while (!finish) {
-            for (Player current : players) {
-                current.makePlay(board);
-                if (checkFinish(current)){
-                    finish=true;
-                    break;
-                }
-                counter++;
-                if (counter==9){
-                    finish = true;
-                    System.out.println("There was a tie, Congratulations to both players");
-                    break;
-                }
-            }
-        }
+        game.showBoard();
+        game.startGame();
     }
-
-    static ArrayList<Player> insertPlayers() {
-        Player player1;
-        Player player2;
-        String auxName;
-        ArrayList<Player> players = new ArrayList<>();
-        System.out.print("Insert first player's name: ");
-        auxName = sc.nextLine();
-        player1 = new Player(auxName, "O");
-        System.out.print("Insert second player's name: ");
-        auxName = sc.nextLine();
-        player2 = new Player(auxName, "X");
-        players.add(player1);
-        players.add(player2);
-        return players;
-    }
-
-    static boolean checkFinish(Player player) {
-        boolean finish = false;
-        ArrayList<Integer> cellIdList = new ArrayList<>();
-
-        if (player.getCellsQty() >= 3) {
-            for (Cell cell : player.getCellsSelected()) {
-                cellIdList.add(cell.getId());
-            }
-            if ((cellIdList.contains(1) && cellIdList.contains(2) && cellIdList.contains(3)) || (cellIdList.contains(4) && cellIdList.contains(5) && cellIdList.contains(6)) || (cellIdList.contains(7) && cellIdList.contains(8) && cellIdList.contains(9))) {
-                finish = true;
-                System.out.println("Congratulations, " + player.getName() + " has Won!!!");
-            }else if ((cellIdList.contains(1) && cellIdList.contains(4) && cellIdList.contains(7)) || (cellIdList.contains(2) && cellIdList.contains(5) && cellIdList.contains(8)) || (cellIdList.contains(3) && cellIdList.contains(6) && cellIdList.contains(9))){
-                finish = true;
-                System.out.println("Congratulations, " + player.getName() + " has Won!!!");
-            }else if ((cellIdList.contains(1) && cellIdList.contains(5) && cellIdList.contains(9)) || (cellIdList.contains(3) && cellIdList.contains(5) && cellIdList.contains(7))){
-                finish = true;
-                System.out.println("Congratulations, " + player.getName() + " has Won!!!");
-            }
-
-        }
-
-        return finish;
-    }
-
 }
